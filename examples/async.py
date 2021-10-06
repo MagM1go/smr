3d465189dd17c869851c2b501bbcc0444b02a8cc
MagM1go <68418026+MagM1go@users.noreply.github.com>
2021-10-06 17:53:07 +0800

    м)

---
+++ examples/async.py
@@ -0,0 +1,15 @@
+from smr_py import smr_async
+import asyncio
+
+
+smr = smr_async.SMR()
+
+async def main():
+    print(await smr.get_point(
+                            category="canvas", 
+                            name="invert",
+                            path="?avatar=https://images-ext-2.discordapp.net/external/0DUf6d_FzJSMhsAXEAoYjtGVbdtvbOa6eM7IeBn6m7g/https/cdn.discordapp.com/avatars/598387707311554570/5eb2fad66a96e41fcb514df8d632a354.png"
+    ))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
--- examples/sync.py
+++ examples/sync.py
@@ -1,4 +1,10 @@
 from smr_py import smr_sync
 
 
-print(smr_sync.SMR_client().invert(avatar_url="avatar_url"))
+smr = smr_sync.SMR_client()
+
+print(smr.get_point(
+                    category="canvas",
+                    name="invert",
+                    path="?avatar=https://images-ext-2.discordapp.net/external/0DUf6d_FzJSMhsAXEAoYjtGVbdtvbOa6eM7IeBn6m7g/https/cdn.discordapp.com/avatars/598387707311554570/5eb2fad66a96e41fcb514df8d632a354.png"
+    ))
