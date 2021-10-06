a0e7e023fb4f96d4447dbf0c145d1d91abdfdfad
MagM1go <68418026+MagM1go@users.noreply.github.com>
2021-10-06 17:49:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
+import pathlib
 
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text(encoding="utf8")
 
 setuptools.setup(
-	name="smr-py",
-	version="1.0.0",
-	author="Eric Chi",
+	name="smr_py",
+	version="1.0.6",
+	author="MagMigo",
 	description="Simple wrapper for some-random-api on python.",
+    long_description=README,
+    long_description_content_type="text/markdown",
 	url="https://github.com/MagM1go/smr",
 	packages=setuptools.find_packages(),
 	classifiers=[
