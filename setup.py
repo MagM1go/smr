705010a3bfddc26edb02a70f28af95541fe56329
MagM1go <68418026+MagM1go@users.noreply.github.com>
2021-10-06 18:26:03 +0800

    ok

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text(encoding="utf8")
 
 setuptools.setup(
-	name="smr_py",
-	version="1.0.9",
+	name="some_random_python",
+	version="1.0.11",
 	author="MagMigo",
 	description="Simple wrapper for some-random-api on python.",
-    long_description=README,
-    long_description_content_type="text/markdown",
+	long_description=README,
+	long_description_content_type="text/markdown",
 	url="https://github.com/MagM1go/smr",
 	packages=setuptools.find_packages(),
 	classifiers=[
@@ -20,5 +20,5 @@ setuptools.setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	python_requires='>=3.6',
+	python_requires='>=3.7',
 )
